d9da90ee513be03bf44c8720daa6893e72182c1d
NijikawaSatoki <120293916+NijikawaSatoki@users.noreply.github.com>
2022-12-11 00:18:25 -0500

    Update make-and-go-dir.sh

--- misc_scripts/make-and-go-dir.sh
+++ misc_scripts/make-and-go-dir.sh
@@ -1,1 +1,15 @@
+#!bin/bash
 
+# [ja-JP]
+# 制作者：虹川郷鬼
+# バージョン：1.00a
+# 
+# [en-US]
+# Author: Satoki Nijikawa
+# Version: 1.00a
+# 
+# A script to make a directory and then go to the new directory
+
+# REPLACE [newdir] WITH DIRECTORY NAME
+mkdir [newdir]
+cd [newdir]
