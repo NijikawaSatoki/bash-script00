66d0c21281487512c4c2a876e28200600be7d58c
NijikawaSatoki <120293916+NijikawaSatoki@users.noreply.github.com>
2022-12-11 00:12:29 -0500

    Create update-termux.sh

---
+++ update-scripts/update-termux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# [ja-JP]
+# 制作者：虹川郷鬼
+# バージョン：1.00a
+# 
+# [en-US]
+# Author: Satoki Nijikawa
+# Version: 1.00a
+# 
+# This script updates Termux. You must have used Fdroid to install Termux
+# before you can use this script.
+
+apt update  # Updates repositories
+apt upgrade # Upgrades installed packages
+echo $' '
+echo $'I recommend upgrading packages at least once every 24 hours.'
+echo $'If you don\'t want to upgrade, then that\'s fine. It\'s on '
+echo $'you, though, if something goes wrong in that scenario.'
